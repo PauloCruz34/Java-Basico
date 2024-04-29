4ca0cc832082c9085fb86abe31c51d4d37bc8ac9
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2024-04-29 10:20:39 -0300

    variaveis

--- DioJava/src/edu/paulo/variaveis/Variaveis.java
+++ DioJava/src/edu/paulo/variaveis/Variaveis.java
@@ -3,7 +3,17 @@ package edu.paulo.variaveis;
 public class Variaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int idade = 17;
+		double salario = 2500.15;
+		boolean rico= true;
+		byte maximo = (byte) 32767; // casting leva para uma previsão  maior
+		short maximo2 = (short)2147483647;
+		long maximo3 = 9223372036854775807L;
+		
+		
+				
+		
+				
 
 	}
 
