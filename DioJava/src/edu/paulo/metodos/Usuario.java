73ecfd70e60379be85c951bff949905091a80efe
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2024-04-30 09:44:28 -0300

    metodos

---
+++ DioJava/src/edu/paulo/metodos/Usuario.java
@@ -0,0 +1,27 @@
+package edu.paulo.metodos;
+
+public class Usuario {
+
+	public static void main(String[] args) throws Exception{
+		SmartTv smartTv = new SmartTv();
+		System.out.println("A Tv está "+ smartTv.ligada);
+		System.out.println("A Tv está sintonizada no "+ smartTv.canal);
+		System.out.println("A Tv está no volume "+ smartTv.volume);
+		
+		smartTv.ligar();
+		System.out.println("A tv está ligada? "+smartTv.ligada);
+		
+		smartTv.aumentarVolume();
+		System.out.println("A Tv está no volume "+ smartTv.volume);
+		smartTv.aumentarVolume();
+		System.out.println("A Tv está no volume "+ smartTv.volume);
+		smartTv.diminuirVolume();
+		System.out.println("A Tv está no volume "+ smartTv.volume);
+		
+		smartTv.avançarCanal();
+		System.out.println("A Tv está sintonizada no "+ smartTv.canal);
+		smartTv.voltarCanal();
+		System.out.println("A Tv está sintonizada no "+ smartTv.canal);
+	}
+
+}
